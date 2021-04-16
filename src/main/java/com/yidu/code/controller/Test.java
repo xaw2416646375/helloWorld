2b326a894086f6f6f935fb49b297818c34ff16fa
xaw2416646375 <2416646375@qq.com>
2021-04-16 16:52:54 +0800

    test文件第一次提交

---
+++ src/main/java/com/yidu/code/controller/Test.java
@@ -0,0 +1,13 @@
+package com.yidu.code.controller;
+
+/**
+ * @author XiongAiWen
+ * @version 1.0
+ * @description:
+ * @data 2021/4/16 16:48
+ */
+public class Test {
+    public void test(){
+
+    }
+}
